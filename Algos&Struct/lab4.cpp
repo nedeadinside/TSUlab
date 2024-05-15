8911d9940b84af97fe75394c07b6d96a0b8ce023
nedeadinside <110976029+nedeadinside@users.noreply.github.com>
2024-05-15 12:47:02 +0700

    new labs

---
+++ Algos&Struct/lab4.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+
+class Thing {
+public:
+    int weight;
+    int price;
+
+    Thing() {
+        weight = rand() % 100 + 1; 
+        price = rand() % 100 + 1;
+    }
+};
+
+
+bool compare(Thing a, Thing b) {
+    double r1 = (double)a.price / a.weight;
+    double r2 = (double)b.price / b.weight;
+    return r1 > r2;
+}
+
+
+int greedybag(vector<Thing>& items, int W) {
+    sort(items.begin(), items.end(), compare);
+
+    int currentWeight = 0;
+    double finalvalue = 0.0;
+
+    for (int i = 0; i < items.size(); i++) {
+        if (currentWeight + items[i].weight <= W) {
+            currentWeight += items[i].weight;
+            finalvalue += items[i].price;
+        }
+    }
+
+    return finalvalue;
+}
+
+
+int backtrack(vector<Thing>& items, int currentWeight, int currentValue, int& bestValue, int W, int index) {
+    if (currentWeight > W) {
+        return -1;
+    }
+
+    if (index >= items.size()) {
+        return -1;
+    }
+
+    if (currentValue > bestValue) {
+        bestValue = currentValue;
+    }
+
+    backtrack(items, currentWeight + items[index].weight, currentValue + items[index].price, bestValue, W, index + 1);
+    backtrack(items, currentWeight, currentValue, bestValue, W, index + 1);
+}
+
+
+int backtrackbag(vector<Thing>& items, int W) {
+    int n = items.size();
+    int currentWeight = 0;
+    int currentValue = 0;
+    int bestValue = 0;
+    int index = 0;
+
+    backtrack(items, currentWeight, currentValue, bestValue, W, index);
+
+    return bestValue;
+}
+
+
+int dynamicbag(int W, vector<int> weights, vector<int> prices, int n) {
+    int i, w;
+    vector<vector<int>> K(n + 1, vector<int>(W + 1));
+
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                K[i][w] = max(prices[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
+
+
+int main() {
+    srand(time(0));
+    int n = 3;
+    int totalWeight = 0;
+
+
+    vector<Thing> items;
+    for (int i = 0; i < n; i++) {
+        Thing item;
+        totalWeight += item.weight;
+        items.push_back(item);
+    }
+
+    int W = totalWeight / 2;
+
+
+    if (n <= 10) {
+        cout << "Items:" << endl;
+        for (const auto& item : items) {
+            cout << "Weight: " << item.weight << ", Price: " << item.price << endl;
+            }
+    }
+
+    
+    vector<int> weights(n);
+    vector<int> prices(n);
+
+
+    for (int i = 0; i < n; i++) {
+        weights[i] = items[i].weight;
+        prices[i] = items[i].price;
+    }
+
+
+    cout << "Greedy algorithm: " << greedybag(items, W) << endl;
+    cout << "Backtrack algorithm: " << backtrackbag(items, W) << endl;
+    cout << "Dynamic programming: " << dynamicbag(W, weights, prices, n) << endl;
+
+    return 0;
+}
---
+++ Parallel/Lab5/lab4.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <math.h>
+
+double f(double x, double y) {
+    return sqrt(x*x + 2*y*y + 2.04)/sqrt(1+y*y)
+}
+
+double integrate(double a, double b, double y, int n) {
+    double h = (b - a) / (3 * n);
+    double sum = 0.0;
+    
+    for (int i = 1; i <= ((3 * n) - 1); i++) {
+        double x = a + i * h;
+        if (i % 3 == 0) {
+            sum += 2 * f(x, y);
+        }
+        else {
+            sum += 3 * f(x, y);
+        }
+    }
+    
+    return ((b - a)/(8 * n))*(f(a) + f(b) + sum);
+}
+
+double doubleIntegrate(double a, double b, double c, double d, int n) {
+    double result = 0.0;
+    
+    for (int i = 1; i <= ((3 * n) - 1); i++) {
+        double y = c + i * h;
+        if (i % 3 == 0) {
+            result += 2 * integrate(a, b, y, n);
+        }
+        else {
+            result += 3 * integrate(a, b, y, n);
+        }
+    }
+    
+    return ((d - c)/(8 * n))*(integrate(a, b, y, n) + integrate(a, b, y, n) + result);
+}
+
+int main(int argc, char** argv) {
+    int rank, size;
+    double a = 0, b = 3;
+    double c = -1, d = 4;
+
+    int n = 1000;
+    
+    double result, local_result;
+    double start_time, end_time;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    int local_n = n / size;
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+        
+    local_result = doubleIntegrate(a + (rank * (b - a) / size), a + ((rank + 1) * (b - a) / size), c + (rank * (d - c) / size), c + ((rank + 1) * (d - c) / size), local_n);
+
+    end_time = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Reduce(&local_result, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    if (rank == 0) {
+	    printf("Size: %d \n", size);
+        printf("Result: %.10f \n", result);
+        printf("Time: %f seconds\n", end_time - start_time);
+    }
+    
+    MPI_Finalize();
+    
+    return 0;
+}
