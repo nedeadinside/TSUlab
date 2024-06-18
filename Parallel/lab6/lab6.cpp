879f2c0cd05811bf1a818e98fa9a88e0544f0033
nedeadinside <110976029+nedeadinside@users.noreply.github.com>
2024-06-18 18:38:45 +0700

    Added parallel prog lab

--- Parallel/lab6/lab6.cpp
+++ Parallel/lab6/lab6.cpp
@@ -1,6 +1,8 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <omp.h>
 #include <math.h>
+
+#define NUM_THREADS 8
 
 double f(double x) {
     return exp(-tan(0.8 * x)) / (1.35 + cos(x));
@@ -9,8 +11,8 @@ double f(double x) {
 double integrate(double a, double b, int n) {
     double h = (b - a) / (3 * n);
     double sum = 0.0;
-    
-    #pragma omp parallel for reduction(+:sum)
+
+#pragma omp parallel for reduction(+:sum) num_threads(NUM_THREADS)
     for (int i = 1; i <= ((3 * n) - 1); i++) {
         double x = a + i * h;
         if (i % 3 == 0) {
@@ -20,28 +22,35 @@ double integrate(double a, double b, int n) {
             sum += 3 * f(x);
         }
     }
-    
-    return ((b - a)/(8 * n))*(f(a) + f(b) + sum);
+
+    return ((b - a) / (8 * n)) * (f(a) + f(b) + sum);
 }
 
 int main(int argc, char** argv) {
     double a = 2.5, b = 5;
-    int n = 1000;
-    
+    int n = 100000;
+    int size = 0;
+
     double result;
     double start_time, end_time;
-    
-    int size = omp_get_max_threads();
-    
-    start_time = omp_get_wtime();
-        
-    result = integrate(a, b, n);
 
-    end_time = omp_get_wtime();
+    #pragma omp parallel num_threads(NUM_THREADS) private(result, start_time, end_time)
+    {
+        start_time = omp_get_wtime();
 
-    printf("Size: %d \n", size);
-    printf("Result: %.10f \n", result);
-    printf("Time: %f seconds\n", end_time - start_time);
-    
+        result = integrate(a, b, n);
+
+        end_time = omp_get_wtime();
+
+        size = NUM_THREADS;
+
+        #pragma omp critical
+        {
+            printf("Size: %d \n", size);
+            printf("Result: %.10f \n", result);
+            printf("Time: %f seconds\n", end_time - start_time);
+        }
+    }
+
     return 0;
 }
